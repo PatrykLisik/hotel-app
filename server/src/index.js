00c69e3d20998579a95ac5f87fa3336fd5162674
plisik <patrykkbkak@gmail.com>
2019-03-28 14:30:18 +0100

    eslint added

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-//Middleware
-app.use(bodyParser.json());
-app.use(cors());
+// Middleware
+app.use(bodyParser.json())
+app.use(cors())
 
-const rooms = require('./routes/api/rooms');
-app.use('/api/rooms', rooms);
+const rooms = require('./routes/api/rooms')
+app.use('/api/rooms', rooms)
 
+const port = process.env.PORT || 5000
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`))
