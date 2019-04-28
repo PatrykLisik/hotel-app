72f79c28d34b3078b51bf606fbfcd81f7ae263d3
Patryk Lisik <patrykkbkak@gmail.com>
2019-04-28 18:03:20 +0200

    old staff deleted, sequelize added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const errorHandler = require('./_helpers/error-handler')
 const morgan = require('morgan')
 
 // Middleware
@@ -12,15 +11,6 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 app.use(cors())
 app.use(morgan('combined'))
-
-// api routes
-app.use('/users', require('./users/users.controller'))
-
-const rooms = require('./routes/api/rooms')
-app.use('/api/rooms', rooms)
-
-// global error handler
-app.use(errorHandler)
 
 // start server
 const port = process.env.PORT || 5000
