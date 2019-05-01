35c7db07a4d9d84a4963ef74067deb2b68d35bbd
Patryk Lisik <patrykkbkak@gmail.com>
2019-05-01 15:09:44 +0200

    full user definition

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,11 @@
 module.exports = (sequelize, DataTypes) => sequelize.define('User', {
+  ID: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  first_name: DataTypes.STRING,
+  last_name: DataTypes.STRING,
   email: {
     type: DataTypes.STRING,
     unique: true
