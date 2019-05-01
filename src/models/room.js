e3b10077e04f7cb258996bcd9f7b9347b19e7675
Patryk Lisik <patrykkbkak@gmail.com>
2019-05-01 20:14:11 +0200

    room and room eq

---
+++ src/models/room.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const Rooms = sequelize.define('Rooms', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    room_number: {
+      type: DataTypes.INTEGER,
+      unique: true
+    },
+    room_floor: DataTypes.INTEGER,
+    type: DataTypes.STRING,
+    room_area: DataTypes.FLOAT,
+    cost: DataTypes.DECIMAL(10, 2),
+    equipment: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Room_Equipments',
+        key: 'id'
+      }
+    }
+  })
+
+  Rooms.associate = (models) => {
+    Rooms.hasOne(models.Rooms_Equipment)
+  }
+  return Rooms
+}
