a540442d154c5d774c53fa69dbb9820efcbbebf9
Patryk Lisik <patrykkbkak@gmail.com>
2019-05-01 16:43:34 +0200

    contact form model

---
+++ src/models/contact_from.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Form = sequelize.define('Contact_Forms', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    text: DataTypes.STRING
+  })
+
+  Form.associate = (models) => {
+    Form.hasOne(models.Users)
+  }
+  return Form
+}
