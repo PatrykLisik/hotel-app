8b5eb358dce38c8ee58db15451c66756e13a7381
Patryk Lisik <patrykkbkak@gmail.com>
2019-05-04 14:47:41 +0200

    room model

---
+++ src/models/room.js
@@ -0,0 +1,14 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+  const Room = sequelize.define('Room', {
+    Number: DataTypes.INTEGER,
+    Floor: DataTypes.INTEGER,
+    PeopleNumber: DataTypes.INTEGER,
+    Type: DataTypes.STRING,
+    cost: DataTypes.DECIMAL(10, 2)
+  }, {})
+  Room.associate = function (models) {
+    // associations can be defined here
+  }
+  return Room
+}
