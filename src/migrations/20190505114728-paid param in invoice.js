8f5e7a1873cd113e8494cd27e92b4d000fcaff4a
Patryk Lisik <patrykkbkak@gmail.com>
2019-05-05 13:53:02 +0200

    paid param in invoice

---
+++ src/migrations/20190505114728-paid param in invoice.js
@@ -0,0 +1,21 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Invoices', 'Paid', {
+      type: Sequelize.BOOLEAN,
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL'
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+}
