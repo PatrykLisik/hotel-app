8871f89057f99536818e026b73896d5f8778f1fc
Patryk Lisik <patrykkbkak@gmail.com>
2019-05-10 20:02:51 +0200

    id is required in put, update and del requests

--- src/policies/IdRequire.js
+++ src/policies/IdRequire.js
@@ -4,24 +4,17 @@ const schema = Joi.object().keys({
   id: Joi.number().integer().required()
 })
 
-function requireIdInJSON (req, res, next, json) {
-  const {
-    error
-  } = Joi.validate(json, schema)
-  if (error) {
-    res.status(400).send({
-      error: 'id is required and must be integer'
-    })
-  } else {
-    next()
-  }
-}
-
 module.exports = {
   requireIdInBody (req, res, next) {
-    requireIdInJSON(req, res, next, req.body)
-  },
-  requireIdInUpdte (req, res, next) {
-    requireIdInJSON(req, res, next, req.body.update)
+    const {
+      error
+    } = Joi.validate(req.body, schema)
+    if (error) {
+      res.status(400).send({
+        error: 'id is required and must be integer'
+      })
+    } else {
+      next()
+    }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
       message: 'test endpoint Hello World!'
     })
   })
-
+  const idPolicy = require('./policies/IdRequire.js')
   // User api
   const userController = require('./Controllers/UserController')
   const userCreationPolicy = require('./policies/UserCreationPolicy')
@@ -15,31 +15,50 @@ module.exports = (app) => {
   )
   app.post('/login', userController.login)
   app.put('/user',
+    idPolicy.requireIdInBody,
     userCreationPolicy.update,
     userController.update)
-  app.delete('/user', userController.delete)
+  app.delete('/user',
+    idPolicy.requireIdInBody,
+    userController.delete)
 
   // Room api
   const roomController = require('./Controllers/RoomController')
 
-  app.get('/room', roomController.getOne)
+  app.get('/room',
+    idPolicy.requireIdInBody,
+    roomController.getOne)
   app.get('/room/all', roomController.getAll)
   app.post('/room', roomController.createOne)
-  app.put('/room', roomController.update)
-  app.delete('/room', roomController.delete)
+  app.put('/room',
+    idPolicy.requireIdInBody,
+    roomController.update)
+  app.delete('/room',
+    idPolicy.requireIdInBody,
+    roomController.delete)
 
   // Reservation api
   const reservationController = require('./Controllers/ReservationController')
   app.post('/reservation', reservationController.createOne)
   app.get('/reservation/all', reservationController.getAll)
-  app.get('/reservation', reservationController.getOne)
-  app.put('/reservation', reservationController.update)
-  app.delete('/reservation', reservationController.delete)
+  app.get('/reservation',
+    idPolicy.requireIdInBody,
+    reservationController.getOne)
+  app.put('/reservation',
+    idPolicy.requireIdInBody,
+    reservationController.update)
+  app.delete('/reservation',
+    idPolicy.requireIdInBody,
+    reservationController.delete)
 
   // Invoice api
   const invoiceController = require('./Controllers/InvoiceController')
   app.post('/invoice', invoiceController.create)
   app.get('/invoice/all', invoiceController.getAll)
-  app.get('/invoice', invoiceController.getOne)
-  app.delete('/invoice', invoiceController.delete)
+  app.get('/invoice',
+    idPolicy.requireIdInBody,
+    invoiceController.getOne)
+  app.delete('/invoice',
+    idPolicy.requireIdInBody,
+    invoiceController.delete)
 }
