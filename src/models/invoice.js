75e964fc4a8ae5ffff03318d6020eb4ff0c0bcc2
Patryk Lisik <patrykkbkak@gmail.com>
2019-05-11 20:57:53 +0200

    payment method is enum, state in model, no paid  bool

--- src/models/invoice.js
+++ src/models/invoice.js
@@ -3,8 +3,14 @@ module.exports = (sequelize, DataTypes) => {
   const Invoice = sequelize.define('Invoice', {
     date: DataTypes.DATEONLY,
     value: DataTypes.DECIMAL,
-    payment_method: DataTypes.STRING,
-    paid: DataTypes.BOOLEAN
+    payment_method: {
+      type: DataTypes.ENUM,
+      values: ['card', 'cash', 'transfer']
+    },
+    state: {
+      type: DataTypes.ENUM,
+      values: ['paid', 'not paid']
+    }
   }, {})
   Invoice.associate = function (models) {
     // associations can be defined here
