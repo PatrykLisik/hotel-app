c519b6e79746c3a1cad8c4947f15f34b03af048b
Patryk Lisik <patrykkbkak@gmail.com>
2019-05-14 09:20:11 +0200

    current state

--- src/Middleware/Authorize.js
+++ src/Middleware/Authorize.js
@@ -1,39 +1,13 @@
 const config = require('../config/config')
 const jwt = require('jsonwebtoken')
 const secret = config.authentication.secret
-const { Reservation } = require('../models')
-
-function returnFalse () {
-  return false
-}
-
-function isUserIdSameInTokenAndBody (req, res, next, tokenPayload) {
-  return req.body.id === tokenPayload.userJSON.id
-}
-
-function isUserAndClientIdSame (req, res, next, tokenPayload) {
-  return req.body.clientId === tokenPayload.userJSON.id
-}
-
-function isAssociatedReservation (req, res, next, tokenPayload) {
-  Reservation.findOne({
-    where: {
-      id: req.body.roomId
-    }
-  }).then(reservation => {
-    if (!tokenPayload.userJSON.id) {
-      return false
-    }
-
-    if (!reservation) {
-      return false
-    }
-    return reservation.clientId === tokenPayload.userJSON.id
-  })
-}
+const roles = require('./Roles')
 
 module.exports = {
-  authorizeFactoryMethod (permissionName, preConditions = returnFalse) {
+  authorizeFactoryMethod (permissionName, roles = []) {
+    if (typeof roles === 'string') {
+      roles = [roles]
+    }
     return (req, res, next) => {
       if (!req.headers.authorization) {
         return res.status(403).json({ error: 'No credentials sent!' })
@@ -41,28 +15,13 @@ module.exports = {
 
       const token = req.headers.authorization.split(' ')[1]
       const payload = jwt.verify(token, secret)
-      const roleJSONElement = payload.roleJSON[permissionName]
+      const role = payload.roleJSON.name
 
-      console.log(payload)
-      console.log('Permission: ' + roleJSONElement)
-
-      if (preConditions(req, res, next, payload)) {
-        return next()
-      }
-      if (roleJSONElement) {
+      if (roles.includes(role) || role === role.Admin) {
         console.log('next')
         return next()
       }
       return res.status(403).json({ error: 'Permission denied' })
     }
-  },
-  isUserIdOrRequirePermission (permissionName) {
-    return this.authorizeFactoryMethod(permissionName, isUserIdSameInTokenAndBody)
-  },
-  isUserAndClientIdSameOrRequirePermission (permissionName) {
-    return this.authorizeFactoryMethod(permissionName, isUserAndClientIdSame)
-  },
-  isAssociatedReservationOrRequirePermission (permissionName) {
-    return this.authorizeFactoryMethod(permissionName, isAssociatedReservation)
   }
 }
