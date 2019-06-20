3374e028c0c3267d366af832be27ece5c3978922
Patryk Lisik <patrykkbkak@gmail.com>
2019-06-20 17:03:27 +0200

    no double password hashing

--- src/models/user.js
+++ src/models/user.js
@@ -35,8 +35,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     hooks: {
       beforeCreate: hashPassword,
-      beforeUpdate: hashPassword,
-      beforeSave: hashPassword
+      beforeUpdate: hashPassword
     }
   })
   User.prototype.comparePassword = function (password) {
